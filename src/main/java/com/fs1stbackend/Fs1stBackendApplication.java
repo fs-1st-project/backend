ccb711fb2be7438191b5375a108eeea25b3dc298
JeongminJamie <sksrhcalsu@gmail.com>
2024-06-10 21:53:51 +0900

    set up spring boot

---
+++ src/main/java/com/fs1stbackend/Fs1stBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fs1stbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fs1stBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fs1stBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fs1stbackend/Fs1stBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs1stbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fs1stBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
