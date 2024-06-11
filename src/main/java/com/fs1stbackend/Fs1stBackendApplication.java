59a92daef66948642f8e228b8b646b416369e4e9
gongjuyeonhee <yyeonhee912@naver.com>
2024-06-11 11:50:14 +0900

    resolved conflicts

--- src/main/java/com/fs1stbackend/Fs1stBackendApplication.java
+++ src/main/java/com/fs1stbackend/Fs1stBackendApplication.java
@@ -1,23 +1,13 @@
 package com.fs1stbackend;
 
-import com.fs1stbackend.service.FirebaseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Fs1stBackendApplication implements CommandLineRunner {
-
-    @Autowired
-    private FirebaseService firebaseService;
+public class Fs1stBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Fs1stBackendApplication.class, args);
     }
-
-    @Override
-    public void run(String... args) throws Exception {
-        firebaseService.fetchDataFromFirebase();
-    }
+// 테스트!
 }
