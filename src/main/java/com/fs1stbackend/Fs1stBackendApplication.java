849d9c57b18d1aa9d94eed1eabe1bf7ee453842f
gongjuyeonhee <yyeonhee912@naver.com>
2024-06-11 11:17:47 +0900

    firebase 초기화 설정

--- src/main/java/com/fs1stbackend/Fs1stBackendApplication.java
+++ src/main/java/com/fs1stbackend/Fs1stBackendApplication.java
@@ -1,13 +1,23 @@
 package com.fs1stbackend;
 
+import com.fs1stbackend.service.FirebaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Fs1stBackendApplication {
+public class Fs1stBackendApplication implements CommandLineRunner {
+
+    @Autowired
+    private FirebaseService firebaseService;
 
     public static void main(String[] args) {
         SpringApplication.run(Fs1stBackendApplication.class, args);
     }
-// 테스트!
+
+    @Override
+    public void run(String... args) throws Exception {
+        firebaseService.fetchDataFromFirebase();
+    }
 }
